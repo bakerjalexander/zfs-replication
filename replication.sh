ce24e596686964b10c216a034c4c778858304f64
bakerjalexander <bakerjalexander@gmail.com>
2022-10-26 00:32:29 -0500

    Update replication.sh

--- replication.sh
+++ replication.sh
@@ -15,14 +15,14 @@ sleep 10 # Give the other system time to make its snapshots
 for sync_to_local in "${remote_datasets[@]}"; do
    [[ ! "$sync_to_local" =~ / ]] && continue
    echo "`date` starting sync of $sync_to_local " >> $log 2>&1
-   /usr/local/sbin/syncoid ${RECVOPTS} --recvoptions="u" --sendoptions="p" --no-sync-snap --quiet \
+   /usr/local/sbin/syncoid --recvoptions="u" --sendoptions="p" --no-sync-snap --quiet \
       -c aes128-gcm@openssh.com -r root@$remote_server:"$sync_to_local" "$sync_to_local" 2>&1 | \
       ts '%a %b %e %H:%M:%S %Z %Y' >> $log 2>&1
    echo "`date` synced $sync_to_local " >> $log 2>&1
    echo "`date` checking for local snapshots not on remote server" >> $log 2>&1
    extra_snaps_array=()
    local_snaps=$(/sbin/zfs list -H -r -s name -o name -t snapshot "$sync_to_local")
-   remote_snaps=$(ssh root@$remote_server zfs list -H -r -s name -o name -t snapshot "$sync_to_local" )
+   remote_snaps=$(ssh root@$remote_server zfs list -H -r -s name -o name -t s${RECVOPTS}napshot "$sync_to_local" )
    for extras in $(comm -23  <(echo "$local_snaps") <(echo "$remote_snaps")); do extra_snaps_array+=( "$extras" ); done
       if [[ -z "$remote_snaps" ]]; then
       echo "`date` Unable to retrieve remote snapshots for $sync_to_local" >> $log 2>&1
